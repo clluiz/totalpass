84df83957b39916eb799ca86b1eaeb914f778829
Cleiton Luiz <cleiton.vanquish@gmail.com>
2019-10-10 20:08:48 -0300

    Apply prettier. Add http-server. Create README.md

---
+++ README.md
@@ -0,0 +1,14 @@
+## Instruções para executar este projeto
+
+Após clonar o projeto, existem duas formas para executá-lo, ambas listadas abaixo
+
+### Testando em modo desenvolvimento
+
+1. Instalar as dependências usando o comando yarn install
+2. Para testar em modo desenvolvimento basta digitar npm start
+
+### Testando em modo produção
+
+1. Caso as dependências não estejam instaladas, usar o comando yarn install
+2. Para fazer o build usar o comando npm run build
+3. Use o comando npm run start:prod
--- src/api/api.test.js
+++ src/api/api.test.js
@@ -5,14 +5,14 @@ describe('Via cep api', () => {
   describe('#getAddressByZipcode(37200000)', () => {
     it('should get city data', async () => {
       const response = await viacep.getAddressByZipcode('37200000');
-      expect(response).toBeDefined()
-      expect(response.localidade).toEqual('Lavras')
+      expect(response).toBeDefined();
+      expect(response.localidade).toEqual('Lavras');
     });
   });
   describe('#getAddressByZipcode with invalid zipCode', () => {
     it('should return error', async () => {
       const response = await viacep.getAddressByZipcode('37200123');
-      expect(response).toBeDefined()
+      expect(response).toBeDefined();
       expect(response.erro).toBe(true);
     });
   });
